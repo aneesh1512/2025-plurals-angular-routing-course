45f654128a49dba11e186ebe33411c8c551f4fea
Lara Newsom <laramnewsom@gmail.com>
2025-08-17 16:22:04 -0500

    Completed code from video

--- src/app/home/sub-section/card/card.component.ts
+++ src/app/home/sub-section/card/card.component.ts
@@ -1,5 +1,7 @@
 import { CurrencyPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DETAIL_ROUTE, PRODUCTS_ROUTE } from 'src/app/app.routes';
 import { Pie } from 'src/app/models/pie';
 import { PieService } from 'src/app/services/pie.service';
 import { CartButtonComponent } from "src/app/shared-ui/cart-button/cart-button.component";
@@ -14,9 +16,10 @@ import { CartButtonComponent } from "src/app/shared-ui/cart-button/cart-button.c
 export class CardComponent {
   readonly pie = input.required<Pie>();
   private readonly pieService = inject(PieService);
+  private readonly router = inject(Router);
 
   selectPie(pieId: string) {
     this.pieService.setSelectedPie(pieId);
-    // route to the detail page
+    this.router.navigate([PRODUCTS_ROUTE, DETAIL_ROUTE])
   }
 }
