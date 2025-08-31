fcc24e8569b993587ecdd2aa49cf338567add064
Lara Newsom <laramnewsom@gmail.com>
2025-08-31 15:32:47 -0500

    bonus code challenge changes

--- src/app/cart/cart-modal/cart-modal.component.ts
+++ src/app/cart/cart-modal/cart-modal.component.ts
@@ -1,22 +1,22 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CartFormComponent } from "../cart-form/cart-form.component";
 import { MatButton } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart-modal',
-  imports: [CartFormComponent, MatButton],
+  imports: [CartFormComponent, MatButton, RouterLink],
   templateUrl: './cart-modal.component.html',
   styleUrl: './cart-modal.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CartModalComponent {
-  router = inject(Router)
+  //router = inject(Router)
 
   close():void {
     // navigate using the router to clear the cart outlet
-    this.router.navigate([{ outlets: { cartModal: null } }], {
-      queryParamsHandling: 'merge',
-    });
+    // this.router.navigate([{ outlets: { cartModal: null } }], {
+    //   queryParamsHandling: 'merge',
+    // });
   }
 }
