532669edb7c35a291c8bc33bb98ca88757359faf
Lara Newsom <laramnewsom@gmail.com>
2025-09-07 23:36:12 -0500

    finished code changes for M7_V3

---
+++ src/app/is-pizza-enabled-can-activate.guard.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { CanActivateFn } from '@angular/router';
+
+import { isPizzaEnabledCanActivateGuard } from './is-pizza-enabled-can-activate.guard';
+
+describe('isPizzaEnabledCanActivateGuard', () => {
+  const executeGuard: CanActivateFn = (...guardParameters) => 
+      TestBed.runInInjectionContext(() => isPizzaEnabledCanActivateGuard(...guardParameters));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  it('should be created', () => {
+    expect(executeGuard).toBeTruthy();
+  });
+});
